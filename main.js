996430ac555282cb144435aa00b8c3f0956c3184
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-03-16 15:23:09 -0500

    Add image import

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 import './style.css' // al estar dentro de main.js que a la vez es el punto de entrada de la aplicación, se importa el archivo de estilos globales
 // importacion de css modules
 import readDocsStyles from './read-docs.module.css'
+import imageStyles from './image.module.css'
 
 import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/vite.svg' // importacion de imagenes
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      <img src="${viteLogo}" class="logo vite" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
@@ -26,5 +27,8 @@ document.querySelector('#app').innerHTML = `
 
 document.querySelector('#read-docs').className = readDocsStyles['read-docs'] // tambien se puede acceder con readDocsStyles.readDocs
 // otra forma es con classList: classList.add(readDocsStyles.readDocs)
+
+document.querySelector('.vite').classList.add(imageStyles.img)
+document.querySelector('.vanilla').classList.add(imageStyles.img)
 
 setupCounter(document.querySelector('#counter'))
